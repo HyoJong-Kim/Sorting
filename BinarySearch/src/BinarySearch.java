4a5ef2f81e09df264b2e02e3dd725692902be93b
HyoJong-Kim <khj8928@gmail.com>
2015-09-23 14:55:05 +0900

    Binary Search

---
+++ BinarySearch/src/BinarySearch.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+/**
+ * Binary Search
+ *
+ * Created by HyoJong on 2015-09-23.
+ */
+public class BinarySearch {
+
+    static int count = 0;
+
+    public static int _BinarySearch(int[] array, int target, int start, int end)
+    {
+        int mid = (start + end) / 2;
+
+        count++;
+
+        if(array[mid] == target)
+            return mid;
+        else
+        {
+            if(target > array[mid])
+               return _BinarySearch(array, target, mid + 1, end);
+            else
+               return _BinarySearch(array, target, start, mid - 1);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Random random = new Random();
+
+        int size = 1 << 2;
+        int index;
+        int target = random.nextInt(size - 1);
+        int[] array = new int[size];
+
+
+        for(int i = 0; i < size; i++)
+        {
+            array[i] = i;
+        }
+
+        index = _BinarySearch(array, target, 0, array.length - 1);
+
+        System.out.println(array[index] + " is " + (index+1) + "th element.");
+        System.out.println("It spent " + count + " times");
+
+    }
+}
