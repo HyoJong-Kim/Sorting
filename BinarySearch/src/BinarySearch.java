e1fd678570c9699c9734c13cc5e7972c16e88704
HyoJong-Kim <khj8928@gmail.com>
2015-09-23 15:14:12 +0900

    Binary Search_v.1

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -13,26 +13,32 @@ public class BinarySearch {
     {
         int mid = (start + end) / 2;
 
-        count++;
 
-        if(array[mid] == target)
-            return mid;
+
+        if(end < start)
+            return -1;
         else
         {
-            if(target > array[mid])
-               return _BinarySearch(array, target, mid + 1, end);
+            count++;
+
+            if (array[mid] == target)
+                return mid;
+            else if (target > array[mid])
+                return _BinarySearch(array, target, mid + 1, end);
             else
-               return _BinarySearch(array, target, start, mid - 1);
+                return _BinarySearch(array, target, start, mid - 1);
+
         }
+
     }
 
     public static void main(String args[])
     {
         Random random = new Random();
 
-        int size = 1 << 2;
+        int size = 1 << 20;
         int index;
-        int target = random.nextInt(size - 1);
+        int target = random.nextInt(size);
         int[] array = new int[size];
 
 
@@ -41,9 +47,12 @@ public class BinarySearch {
             array[i] = i;
         }
 
-        index = _BinarySearch(array, target, 0, array.length - 1);
 
-        System.out.println(array[index] + " is " + (index+1) + "th element.");
+        if((index = _BinarySearch(array, target, 0, array.length - 1)) != -1)
+            System.out.println(array[index] + " is " + (index+1) + "th element.");
+        else
+            System.out.println("Target is not exist");
+
         System.out.println("It spent " + count + " times");
 
     }
